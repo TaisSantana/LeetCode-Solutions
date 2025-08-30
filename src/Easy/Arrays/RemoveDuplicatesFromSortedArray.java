a35ff4e9ddb4c83233763289b4edd345d5afd954
Tatah <tais_santana002@hotmail.com>
2025-08-30 19:19:56 -0300

    Building a folder structure and the Remove Duplicates From Sorted Array exercice

---
+++ src/Easy/Arrays/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,41 @@
+package Easy.Arrays;
+
+/*
+Problem 26 - Remove Duplicates from Sorted Array
+https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+Description:
+Given a sorted array nums, remove the duplicates in-place such that each element appears only once
+and returns the new length. Do not allocate extra space for another array; you must do this by
+modifying the input array in-place with O(1) extra memory.
+
+Solution:
+- Use two pointers approach:
+    1. lastNumberFound tracks the last unique number seen.
+    2. insertPosition tracks the position to insert the next unique number.
+- Iterate through the array, and if the current number is different from lastNumberFound,
+  place it at insertPosition and update pointers.
+- Time complexity: O(n)
+- Space complexity: O(1)
+
+Example:
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+*/
+
+public class RemoveDuplicatesFromSortedArray {
+    public int removeDuplicates(int[] nums) {
+        int lastNumberFound = nums[0] - 1;
+        int insertPosition = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != lastNumberFound) {
+                nums[insertPosition] = nums[i];
+                lastNumberFound = nums[i];
+                insertPosition++;
+            }
+        }
+
+        return insertPosition;
+    }
+}
