d9b97d06f84ee51167f49e83b3666d68d49b0b1f
Tatah <tais_santana002@hotmail.com>
2025-08-30 19:37:33 -0300

    Solve BestTimeToBuyAndSellStockII Exercice

---
+++ src/Easy/Arrays/BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,39 @@
+package Easy.Arrays;
+
+/*
+Problem 122 - Best Time to Buy and Sell Stock II
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+
+Description:
+You are given an array prices where prices[i] is the price of a given stock on day i.
+You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times),
+but you can only hold at most one share of the stock at any time. You can buy and sell on the same day.
+
+        Solution:
+        - Iterate through the array and sum all positive differences between consecutive days.
+        - This ensures you "collect" all profitable increases.
+- Time complexity: O(n)
+- Space complexity: O(1)
+
+Example:
+Input: prices = [7,1,5,3,6,4]
+Output: 7
+Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 4.
+Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 3.
+Total profit = 7.
+        */
+
+public class BestTimeToBuyAndSellStockII {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+
+        return profit;
+    }
+
+}
